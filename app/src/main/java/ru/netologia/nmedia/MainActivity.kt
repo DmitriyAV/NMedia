65697e2e1abdd4bd4bc2a1ad6ebd91349d18b85b
DmitriyAV <dima.alekceev@gmail.com>
2021-11-17 11:34:19 +0300

    MVVM fix

--- app/src/main/java/ru/netologia/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netologia/nmedia/MainActivity.kt
@@ -2,13 +2,11 @@ package ru.netologia.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import ru.netologia.nmedia.ViewModel.ViewModelPost
 import ru.netologia.nmedia.databinding.ActivityMainBinding
-import ru.netologia.nmedia.dto.Post
 import ru.netologia.nmedia.dto.PostService
-import ru.netologia.nmedia.repository.PostRepository
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,24 +22,19 @@ class MainActivity : AppCompatActivity() {
                 author.text = post.author
                 published.text = post.published
                 content.text = post.content
+                likeCount?.text = PostService.checkCounter(post.like)
+                shareCount?.text = PostService.checkCounter(post.sher)
 
                 like.setImageResource(
-                if (post.likedByMe) { R.drawable.ic_liked_24 }
-                else { R.drawable.ic_baseline_favorite_border_24 })
-
-            }
-
-            biding.like.setOnClickListener {
-                Log.d("click Like", "like")
-                viewModel.like()
-                biding.likeCount?.text = PostService.checkCounter(post.like)
-            }
-
-            biding.share.setOnClickListener {
-                Log.d("click share", "share")
-                viewModel.share()
-                biding.shareCount?.text = PostService.checkCounter(post.sher)
+                    if (post.likedByMe) {
+                        R.drawable.ic_liked_24
+                    } else {
+                        R.drawable.ic_baseline_favorite_border_24
+                    }
+                )
             }
         }
+        biding.like.setOnClickListener { viewModel.like() }
+        biding.share.setOnClickListener { viewModel.share() }
     }
 }
